172676b7f343ea1795a7bbc3ea27894a010af05f
lygris <lygriz@gmail.com>
2025-02-22 12:25:58 -0600

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM haproxy:alpine
+RUN apk add --no-cache certbot openssl bash certbot-nginx
+COPY certbot_entrypoint.sh /certbot_entrypoint.sh
+RUN chmod +x /certbot_entrypoint.sh
+
+VOLUME /etc/letsencrypt
+EXPOSE 80 443
+ENTRYPOINT ["/certbot_entrypoint.sh"]
---
+++ certbot-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Check if certificates exist, and obtain if not
+if [ ! -f /etc/letsencrypt/live/$domain/combined.pem ]; then
+    echo "Obtaining certificates..."
+    certbot certonly --standalone --agree-tos --no-eff-email --email $email -d $domain
+    if [ $? -ne 0 ]; then
+        echo "Failed to obtain certificates, exiting..."
+        exit 1
+    fi
+    cat /etc/letsencrypt/live/yourdomain.com/fullchain.pem /etc/letsencrypt/live/yourdomain.com/privkey.pem > /etc/letsencrypt/live/yourdomain.com/combined.pem
+fi
+
+# Start HAProxy
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg
